acf01913512cb65c602caf522f110bfe7c487184
khoatn2003 <khoatiennguyen14@gmail.com>
2025-03-06 12:39:05 +0700

    TrangHome

---
+++ TestBTL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestBTL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
